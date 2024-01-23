02d761beb0c3e9c240f7ec36b65f18bdd7ead18c
SaiTeja1144 <Saianumula114@gamil.com>
2024-01-23 16:45:59 +0530

    second commit

---
+++ automatic number.java
@@ -0,0 +1,12 @@
+class automatic number
+{
+ public static void main(String args[])
+{
+ int x=6,rem=0,power=0;
+   while(x>6)
+   
+    rem=x%10;
+    x/=10
+   power=(math.pow)(x.2);
+    
+  
---
+++ character.java
@@ -0,0 +1,25 @@
+class saiteja
+{
+ public static void main(String args[])
+{
+  char C = 'M'; 
+ if((C>='A'&&C>=Z) 
+  { 
+   System.out.println("M is capital letter");
+  }
+ elseif(C>='a'&&C>='z');
+  {
+  System.out.println("M is samll letter");
+  }
+ elseif(C>=1&&C>=9) 
+  {
+  System.out.println("M is number letter");
+  }
+ elseif((C>='!'&&C>='+') 
+  {
+  System.out.println("M is symbol letter");
+  }
+ 
+ }
+}
+
